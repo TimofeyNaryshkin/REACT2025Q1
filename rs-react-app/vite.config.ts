84c40cbc347ed1284951143d56fcafa487dba282
Timofey <naryshkint4@gmail.com>
2025-02-06 00:08:46 +0400

    build: install gh-pages

--- rs-react-app/vite.config.ts
+++ rs-react-app/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/REACT2025Q1/', // совпадает с именем репозитория
+  build: {
+    outDir: 'dist',
+  },
 });
