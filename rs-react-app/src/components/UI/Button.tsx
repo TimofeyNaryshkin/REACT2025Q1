40c8ed222121da93524f85fe108808888fadbb19
Timofey <naryshkint4@gmail.com>
2025-02-06 23:29:33 +0400

    feat: make loader a functional component

--- rs-react-app/src/components/UI/Button.tsx
+++ rs-react-app/src/components/UI/Button.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { ButtonProps } from '../../types/types';
 
-
-
 const Button: React.FC<ButtonProps> = ({ children, onButtonClick }) => {
   return <button onClick={onButtonClick}>{children}</button>;
 };
