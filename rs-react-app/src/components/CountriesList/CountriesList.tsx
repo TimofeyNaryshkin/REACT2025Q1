b6d172e6a6dc9782e95ffdb64a4eee03644decb4
Timofey <naryshkint4@gmail.com>
2025-03-19 22:16:20 +0400

    feat: implement sorting on population

--- rs-react-app/src/components/CountriesList/CountriesList.tsx
+++ rs-react-app/src/components/CountriesList/CountriesList.tsx
@@ -10,6 +10,8 @@ export default function CountriesList() {
   const [countries, setCountries] = useState<Country[]>([]);
   const [filteredCountries, setFlteredCountries] = useState<Country[]>([]);
   const [region, setRegion] = useState<string>('All');
+  const [sortOrder, setSortOrder] = useState<string>('descending');
+  const [sortedCountries, setSortedCountries] = useState<Country[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,15 +31,39 @@ export default function CountriesList() {
       setFlteredCountries(results);
     }
   }, [region, countries]);
+
+  useEffect(() => {
+    if (sortOrder === 'ascending') {
+      const ascendingSorted = [...filteredCountries].sort(
+        (a, b) => a.population - b.population
+      );
+      setSortedCountries(ascendingSorted);
+    }
+    if (sortOrder === 'descending') {
+      const descendingSorted = [...filteredCountries].sort(
+        (a, b) => b.population - a.population
+      );
+      setSortedCountries(descendingSorted);
+    }
+  }, [sortOrder, filteredCountries, countries]);
+
   return (
     <>
       <div className={classes.header}>
         <strong>Name</strong>
-        <strong>Population</strong>
+        <button
+          onClick={() => {
+            setSortOrder(
+              sortOrder === 'ascending' ? 'descending' : 'ascending'
+            );
+          }}
+        >
+          Population
+        </button>
         <Filter regions={regions} onChange={setRegion} />
         <strong>Flag</strong>
       </div>
-      {filteredCountries.map((country) => (
+      {sortedCountries.map((country) => (
         <CountryItem key={country.name.common} country={country} />
       ))}
     </>
